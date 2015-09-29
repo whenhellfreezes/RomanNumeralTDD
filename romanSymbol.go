4a834695dc21f954a016ddfc6958049c60bc03b4
Nicholas Sorenson <soren07@vt.edu>
2015-09-28 22:35:19 -0400

    Added in common symbols.

--- romanSymbol.go
+++ romanSymbol.go
@@ -1,6 +1,16 @@
 // Package roman provides ...
 package roman
 
+var (
+	romanOne         = romanSymbol{1, "I"}
+	romanFive        = romanSymbol{5, "V"}
+	romanTen         = romanSymbol{10, "X"}
+	romanFifty       = romanSymbol{50, "L"}
+	romanHundred     = romanSymbol{100, "C"}
+	romanFiveHundred = romanSymbol{500, "D"}
+	romanThousand    = romanSymbol{1000, "M"}
+)
+
 type romanSymbol struct {
 	value     int
 	character string
