258febc3f1da008326711ff13e2aab333c881fd2
Nicholas Sorenson <soren07@vt.edu>
2015-09-29 18:23:33 -0400

    Changed function to determine whether we are power of ten. Added getLevel

--- romanSymbol.go
+++ romanSymbol.go
@@ -1,5 +1,8 @@
-// Package roman provides ...
 package roman
+
+import (
+	"math"
+)
 
 var (
 	romanOne         = romanSymbol{1, "I"}
@@ -9,16 +12,54 @@ var (
 	romanHundred     = romanSymbol{100, "C"}
 	romanFiveHundred = romanSymbol{500, "D"}
 	romanThousand    = romanSymbol{1000, "M"}
+	romanDict        = DefaultRomanDictionary()
 )
+
+type romanDictionary []romanSymbol
+
+func (slice romanDictionary) Less(i, j int) bool {
+	return slice[i].value < slice[j].value
+}
+
+func (slice romanDictionary) Len() int {
+	return len(slice)
+}
+
+func (slice romanDictionary) Swap(i, j int) {
+	slice[i], slice[j] = slice[j], slice[i]
+}
+
+func DefaultRomanDictionary() romanDictionary {
+	container := make([]romanSymbol, 7)
+	container[0] = romanOne
+	container[1] = romanFive
+	container[2] = romanTen
+	container[3] = romanFifty
+	container[4] = romanHundred
+	container[5] = romanFiveHundred
+	container[6] = romanThousand
+	return container
+}
 
 type romanSymbol struct {
 	value     int
 	character string
 }
 
-func (r romanSymbol) isMultiple() bool {
-	if r.value%10 == 0 || r.value == 1 {
-		return true
+func (r romanSymbol) getLevel() int {
+	for k, v := range romanDict {
+		if v.value == r.value {
+			return k + 1
+		}
+	}
+	return -1
+}
+
+func (r romanSymbol) isPowerOfTen() bool {
+	for i := 0; i < 4; i++ {
+		if int(math.Pow10(i)) == r.value {
+			return true
+		}
 	}
 	return false
 }
--- symbol_test.go
+++ symbol_test.go
@@ -4,20 +4,38 @@ import (
 	"testing"
 )
 
-func TestIsMultiple(t *testing.T) {
+func TestIsPowerOfTen(t *testing.T) {
 	one := romanSymbol{1, "I"}
 	ten := romanSymbol{10, "X"}
 	five := romanSymbol{5, "V"}
-	if !one.isMultiple() {
-		t.Error("One not a multiple")
+	if !one.isPowerOfTen() {
+		t.Error("One should be a power of ten")
 		t.Fail()
 	}
-	if !ten.isMultiple() {
-		t.Error("Ten not a multiple")
+	if !ten.isPowerOfTen() {
+		t.Error("Ten should be a power of ten")
 		t.Fail()
 	}
-	if five.isMultiple() {
-		t.Error("Five counted as a multiple")
+	if five.isPowerOfTen() {
+		t.Error("Five counted as a power of ten")
 		t.Fail()
+	}
+	if !romanThousand.isPowerOfTen() {
+		t.Error("One thousand should be a power of ten")
+		t.Fail()
+	}
+}
+
+func TestLevelTest(t *testing.T) {
+	if !(romanOne.getLevel() == 1) {
+		t.Error("I is not showing as level 1")
+		t.Fail()
+	}
+	if romanFive.getLevel() == 4 {
+		t.Error("romanFive should not be level 4")
+		t.Fail()
+	}
+	if romanFiveHundred.getLevel() != 6 {
+		t.Error("D is not showing as level 6")
 	}
 }
